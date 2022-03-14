05d0dd1361b6ef77ced02b179da0ba584c4dff8f
Kyle Bruxvoort <kbruxvoort@gmail.com>
2022-03-14 09:40:05 -0500

    refactor init to use new settings

--- fetchbim/__init__.py
+++ fetchbim/__init__.py
@@ -1,43 +1,34 @@
-import os
 import httpx
 
-from pydantic import BaseSettings
-from dotenv import load_dotenv
 
+from .config import settings
 from notion_client import AsyncClient, Client
 
-load_dotenv()
-
-BIM_KEY = os.getenv("BIM_KEY")
-DEV_KEY = os.getenv("DEV_KEY")
-NOTION_KEY = os.getenv("NOTION_KEY")
-TEAMS_WEBHOOK = os.getenv("TEAMS_WEBHOOK")
-
-
-class APIKeyMissingError(Exception):
-    pass
-
-
-if BIM_KEY is None:
-    raise APIKeyMissingError("All methods require an API key.")
 
 BIM_HEADERS = {
-    "Authorization": "Bearer {}".format(BIM_KEY),
+    "Authorization": "Bearer {}".format(settings.bim_auth_key),
     "Content-Type": "application/json",
 }
 DEV_HEADERS = {
-    "Authorization": "Bearer {}".format(DEV_KEY),
+    "Authorization": "Bearer {}".format(settings.dev_auth_key),
     "Content-Type": "application/json",
 }
 
 
-client = httpx.Client(base_url="https://www.ssgbim.com/api/", headers=BIM_HEADERS)
-dev_client = httpx.Client(base_url="https://fetch.devssg.com/api/", headers=DEV_HEADERS)
-async_notion = AsyncClient(auth=NOTION_KEY)
-notion = Client(auth=NOTION_KEY)
+client = httpx.Client(base_url=settings.bim_base_url, headers=BIM_HEADERS)
+dev_client = httpx.Client(base_url=settings.dev_base_url, headers=DEV_HEADERS)
+async_notion = AsyncClient(auth=settings.notion_auth_key)
+notion = Client(auth=settings.notion_auth_key)
 
 
-from .models.fetch import (
+from .fetch import (
+    Status,
+    LoadMethod,
+    ObjectType,
+    ParameterType,
+    AttributeType,
+    DataType,
+    MatchType,
     Property,
     Parameter,
     File,
