27eb6b9645060b9db57895c27593b4336b463444
Kyle Bruxvoort <kbruxvoort@gmail.com>
2022-03-21 16:11:27 -0500

    add family example

---
+++ samples/family_example.py
@@ -0,0 +1,41 @@
+from pprint import pprint
+
+from fetchbim.fetch import (
+    Family,
+    FamilyType,
+    Parameter,
+    Property,
+    ParameterType,
+    DataType,
+)
+
+
+props = [
+    Property(name="TitleHeader", value="Created"),
+    Property(name="TitleSubheader", value="Family"),
+]
+
+params = [
+    Parameter(
+        name="Width",
+        value="5.0",
+        parameter_type=ParameterType.INST,
+        data_type=DataType.LENGTH,
+    ),
+    Parameter(name="Manufacturer", value="Kyle"),
+]
+
+type_params = [Parameter(name="Depth", value="2.5", data_type=DataType.LENGTH)]
+
+family_types = [FamilyType(name="Default", is_default=True, parameters=type_params)]
+
+fam = Family(
+    name="New Created Family",
+    category_name="Casework",
+    parameters=params,
+    properties=props,
+    family_types=family_types,
+)
+
+response = fam.create()
+pprint(response)
