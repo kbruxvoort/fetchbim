83515dc8a52e71b824b8bdbefff304db35f8c634
Kyle Bruxvoort <kbruxvoort@gmail.com>
2022-03-24 11:02:37 -0500

    add paginated query function

--- fetchbim/notion/__init__.py
+++ fetchbim/notion/__init__.py
@@ -1,3 +1,4 @@
+from ..notion.utils import get_all_pages
 from ..notion.database import Database
 from ..notion.file_emoji import ExternalFile, HostedFile, FileObject, EmojiObject
 from ..notion.page import Page
--- fetchbim/notion/utils.py
+++ fetchbim/notion/utils.py
@@ -1,35 +1,5 @@
-from enum import Enum
-
-
-class Condition(Enum):
-    EQUALS = "equals"
-    NOT_EQUAL = "does_not_equal"
-    CONTAINS = "contains"
-    NOT_CONTAIN = "does_not_contain"
-    STARTS = "starts_with"
-    ENDS = "ends_with"
-    EMPTY = "is_empty"
-    NOT_EMPTY = "is_not_empty"
-
-    def __str__(self):
-        return self.value
-
-
-class PropertyType(Enum):
-    TEXT = "rich_text"
-    NUMBER = "number"
-    BOOL = "checkbox"
-    SELECT = "select"
-    MULTISELECT = "multi_select"
-    DATE = "date"
-    PEOPLE = "people"
-    FILES = "files"
-    RELATION = "relation"
-    FORMULA = "formula"
-    TITLE = "title"
-
-    def __str__(self):
-        return self.value
+from fetchbim import n_client
+from .page import Page
 
 
 def truncate(value: str, limit: int = 2000) -> str:
@@ -38,25 +8,17 @@ def truncate(value: str, limit: int = 2000) -> str:
     return value
 
 
-# class Property(FetchModel):
-#     pass
+def get_all_pages(database_id: str, **kwargs) -> list[Page]:
+    results = []
+    cursor = None
+    while True:
+        response = n_client.databases.query(
+            database_id=database_id, start_cursor=cursor
+        )
+        results.extend(response["results"])
+        if response["has_more"] is False:
+            break
+        else:
+            cursor = response["next_cursor"]
 
-
-# class Page(FetchModel):
-#     @staticmethod
-#     def get_title(dct):
-#         title = None
-#         props = dct.get("properties")
-#         if props:
-#             for p in props:
-#                 if props[p]["type"] == "title":
-#                     title = props[p]["title"][0]["plain_text"]
-#         return title
-
-#     @staticmethod
-#     def archive(page_id):
-#         notion.pages.update(page_id=page_id, **{"archived": True})
-
-#     @staticmethod
-#     def restore(page_id):
-#         notion.pages.update(page_id=page_id, **{"archived": False})
+    return [Page(**result) for result in results]
